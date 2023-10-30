d604cd1083fbe8d46900cc92e0cc8a3a16ec7199
MadelineMo <mmoran11@luc.edu>
2023-10-30 12:25:03 -0500

    design edits (slide show now center)

--- slideshow.js
+++ slideshow.js
@@ -7,10 +7,14 @@ function showSlides() {
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
+  // next slide
   slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
+  // start at the begining
+  if (slideIndex > slides.length) {
+    slideIndex = 1
+  }
   slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 5000); // Change image every 2 seconds
+  setTimeout(showSlides, 5000); // Change image every 5 seconds
 }
 
 //source: https://www.w3schools.com/howto/howto_js_slideshow.asp
